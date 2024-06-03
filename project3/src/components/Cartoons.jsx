0e1b8934592bc30cd532f03dbbd86830249780f1
Nehaa7255 <nehamas04@gmail.com>
2024-06-03 15:48:59 +0530

    Project 3 completed.

--- project3/src/components/Cartoons.jsx
+++ project3/src/components/Cartoons.jsx
@@ -1,19 +1,22 @@
-import {cartoons} from "../utils/data"
-const Cartoons = ({ cartoons }) => {
-    const Cartoon = cartoons.filter(
-        (cartoon) => cartoon.magnitude % 2 == 0
-      );
-  return (
-    <>
-    <div>
-    <h1>Cartoons Names</h1>
-    {Cartoon.map((cartoon) => (
-        <h1 key={cartoon.id}>{cartoon.name}</h1>
-    ))}
-    
-    </div>
-    </>
-  )
-}
+import {cartoons} from "../utils/data";
 
-export default Cartoons
+const Cartoon = () => {
+  const filterCartoon = cartoons.filter((cartoon) => cartoon.magnitude % 2==0);
+  return (
+    <>   
+     <div>
+      <h1>Cartoon Character Names</h1>
+  {filterCartoon.map((cartoon) => (
+          <div key={cartoon}>
+          <div>
+          <h1  className="name">{cartoon.name}</h1>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+
+  );
+};
+
+export default Cartoon;
